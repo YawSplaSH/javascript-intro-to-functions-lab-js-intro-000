bfa4d0314c88bd27071e496cf5cb2a02afe7c4d9
Jose Segura <josesegura535@gmail.com>
2020-11-05 02:26:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+    function shout(string) {
+     return string.toUpperCase()
+  }
+    function logShout(string) {
+      console.log(string.toUpperCase())
+    }
+
+    function whisper(string) {
+      return string.toLowerCase()
+    }
+    function logWhisper(string) {
+      console.log(string.toLowerCase())
+    }
+
+    function sayHiToGrandma(string) {
+    var lowercase = "hello!"
+   if(lowercase.toLowerCase("hello!") === lowercase)
+   return "I can't hear you!"
+   var uppercase = "HELLO!"
+   if(uppercase.toUpperCase("HELLO!")=== uppercase)
+   return "YES INDEED!"
+
+
+
+  }
